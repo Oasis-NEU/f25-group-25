14b89e4a1a85b17b8c33dcd607bbbae26c1af0b2
Richard Feng <richardfeng43@gmail.com>
2025-10-05 15:43:40 -0400

    added text over the tables

--- my-react-app/src/navbar.jsx
+++ my-react-app/src/navbar.jsx
@@ -1,10 +1,10 @@
 function NavBar() {
     return <div className='flex justify-center sticky top-0 z-10 w-full'>
         <div className="flex border-b-2 h-20 w-full justify-around items-center">
-            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all">Logo</button>
-            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all">Account</button>
-            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all">Dashboard</button>
-            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all">Add Friends/Groups</button>
+            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all ease-in-out">Logo</button>
+            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all ease-in-out">Account</button>
+            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all ease-in-out">Dashboard</button>
+            <button className="hover:outline-2 rounded-md hover:scale-[1.05] px-8 py-4 transition-all ease-in-out">Add Friends/Groups</button>
         </div>
     </div>
 
